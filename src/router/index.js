3e5dbbf69a2ec506727e232feaac574d7878e826
Alireza <alirezamires@gmail.com>
2024-04-19 00:50:36 +0330

    remove use less files

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 import Login from "@/views/LoginPage.vue";
 import SingUp from "@/views/SingUpPage.vue";
 import Genre from "@/views/GenrePage.vue";
