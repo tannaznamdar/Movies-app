23e51737f3cd521b2d877c873d16bc7cd55b7e4a
tannaznamdar <tannaznamdar99@gmail.com>
2024-04-23 13:29:01 +0330

    creat new search box

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,41 +1,239 @@
 <template>
 
-    <div class="wrapper">
+    <main class="wrapper">
         <div class="container bv-example-row">
-            <div class="row mt-4">
-                
-                <div class="col-lg-3">
-                    <article class="card-container" >
-                        <a href="#">
-                            <figure class="card-img">
-                                <img alt="RebelMoon" :src="RebelMoon">
-                            </figure>
+            <section>
+                <div class="row mt-4">
 
-                            <div class="card-info-overlay">
-                                <div class="card-info">
-                                    <div class="mb-2 d-flex flex-row">
-                                        <span class="internal-distance-l">
-                                            <font-awesome-icon icon="heart" />
-                                            86%
-                                        </span>
-                                        <span>
-                                            <img alt="Imdb" :src="Imdb">
-                                            10/ 5.1
-                                        </span>
+                    <div class="col-lg-3">
+                        <article class="card-container">
+                            <a href="#">
+                                <figure class="card-img">
+                                    <img alt="RebelMoon" :src="RebelMoon">
+                                </figure>
+
+                                <div class="card-info-overlay">
+                                    <div class="card-info">
+                                        <div class="mb-2 d-flex flex-row">
+                                            <span class="internal-distance-l">
+                                                <font-awesome-icon icon="heart" />
+                                                86%
+                                            </span>
+                                            <span>
+                                                <img alt="Imdb" :src="Imdb">
+                                                10/ 5.1
+                                            </span>
+                                        </div>
+
+                                        <h2 class="card-font card-font--title"> فیلم ربل مون 2 </h2>
+                                        <p class="card-font card-font--subTitle"> Rebel Moon 2 2024 </p>
+                                        <span class="button button-transparent-orange">زیرنویس چسبیده</span>
                                     </div>
-
-                                    <h2 class="card-font card-font--title">  فیلم ربل مون 2  </h2>
-                                    <p class="card-font card-font--subTitle"> Rebel Moon 2 2024 </p>
-                                    <span class="transparent-btn transparent-btn-orange">زیرنویس چسبیده</span>
                                 </div>
-                            </div>
-                        </a>
-                    </article>
+                            </a>
+                        </article>
+                    </div>
+
                 </div>
 
-            </div>
+                <div class="new-search-box ">
+                    <form action="#" method="get">
+                        <input type="text" style="display: none;">
+                        <div class="row">
+
+                            <div class="col-lg-2">
+                                <div class="search-box-list">
+                                    <button class="dropbtn d-flex justify-content-between">
+                                        سریال
+                                        <font-awesome-icon class="angle-left-icon" icon="angle-left" />
+                                    </button>
+
+                                    <div class="dropdown-content ">
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                همه
+                                            </label>
+                                        </div>
+
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                فیلم
+                                            </label>
+                                        </div>
+
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                سریال
+                                            </label>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2">
+                                <div class="search-box-list">
+                                    <button class="dropbtn d-flex justify-content-between">
+                                        ژانر
+                                        <font-awesome-icon class="angle-left-icon" icon="angle-left" />
+                                    </button>
+
+                                    <div class="dropdown-content ">
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                همه
+                                            </label>
+                                        </div>
+
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                فیلم
+                                            </label>
+                                        </div>
+
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                سریال
+                                            </label>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2">
+                                <div class="search-box-list">
+                                    <button class="dropbtn d-flex justify-content-between">
+                                        کشور
+                                        <font-awesome-icon class="angle-left-icon" icon="angle-left" />
+                                    </button>
+
+                                    <div class="dropdown-content ">
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                همه
+                                            </label>
+                                        </div>
+
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                فیلم
+                                            </label>
+                                        </div>
+
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                سریال
+                                            </label>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2">
+                                <div class="search-box-list">
+                                    <button class="dropbtn d-flex justify-content-between">
+                                        دوبله و زیر نویس
+                                        <font-awesome-icon class="angle-left-icon" icon="angle-left" />
+                                    </button>
+
+                                    <div class="dropdown-content ">
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                همه
+                                            </label>
+                                        </div>
+
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                فیلم
+                                            </label>
+                                        </div>
+
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                سریال
+                                            </label>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2">
+                                <div class="search-box-list">
+                                    <button class="dropbtn d-flex justify-content-between">
+                                        امتیاز
+                                        <font-awesome-icon class="angle-left-icon" icon="angle-left" />
+                                    </button>
+
+                                    <div class="dropdown-content ">
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                همه
+                                            </label>
+                                        </div>
+
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                فیلم
+                                            </label>
+                                        </div>
+
+                                        <div class="text-item  align-item-center">
+                                            <label class="custom-radio" name="cat-all" value>
+                                                <input checked="checked" name="radio" type="radio">
+                                                <span class="checkmark"></span>
+                                                سریال
+                                            </label>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2">
+                                <button class="button button--lg " type="submit" > جستجو </button>
+                            </div>
+
+                        </div>
+                    </form>
+
+                </div>
+
+
+            </section>
         </div>
-    </div>
+    </main>
 
 </template>
 
@@ -43,13 +241,13 @@
 <script>
 
 import RebelMoon from '../assets/images/RebelMoon.jpg'
+
 import Imdb from '../assets/images/la--imdb.svg'
 
 
 export default {
     name: 'cards',
 
-  
     data() {
         return {
             RebelMoon,
@@ -64,13 +262,13 @@ export default {
 
 
 <style scoped lang="scss">
-
-.card-container{
+.card-container {
     box-shadow: 0 5px 25px rgba(0, 0, 0, .13);
     position: relative;
-    a{
+
+    a {
         text-decoration: none;
-    color: #fff;
+        color: #fff;
     }
 }
 
@@ -97,33 +295,33 @@ export default {
 
 .card-info-overlay {
 
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 100%;
-  width: 100%;
-  opacity: 0;
-  transition: .5s ease;
-  background: rgba(0, 0, 0, .7);
-  border-radius: 4px;
-  transition: all 400ms;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    transition: .5s ease;
+    background: rgba(0, 0, 0, .7);
+    border-radius: 4px;
+    transition: all 400ms;
 }
 
-.card-info{
-  position: absolute;
-  top: 50%;
-  right: 0;
-  -webkit-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-  box-shadow: 0 5px 25px rgba(0, 0, 0, .13) !important;
+.card-info {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    box-shadow: 0 5px 25px rgba(0, 0, 0, .13) !important;
 }
 
 
 .card-container:hover .card-info-overlay {
-  opacity: 1;
+    opacity: 1;
 }
 
 .internal-distance-l {
@@ -146,28 +344,151 @@ export default {
     }
 }
 
-.transparent-btn {
+.button {
     display: inline-block;
     padding: 8px 14px 8px;
     font-size: 12px;
     font-weight: 400;
     line-height: 1;
     text-align: center;
-    white-space: nowrap;
-    vertical-align: baseline;
     border-radius: 3px;
-    border: 1px solid #f0f3f9;
+    white-space: nowrap;
+    vertical-align: middle;
+    user-select: none;
+    border: 1px solid transparent;
 
-    &-orange {
+
+    &-transparent-orange {
+        background: transparent;
         border-color: #FF7555;
         color: #FF7555;
     }
 
-    &-green {
+    &-transparent-green {
+        background: transparent;
         border-color: #89d64f;
         color: #89d64f;
     }
 
-
+    &--lg {
+        color: #fff;
+        background-color: #6898f8;
+        border-color: #6898f8;
+        width: 100%;
+        font-size: 14px;
+        line-height: 2;
+    }
 }
+
+//search-box 
+
+.new-search-box {
+    padding: 10px;
+    background-color: #23232b !important;
+    border-radius: 5px;
+    margin-bottom: 30px;
+    display: block;
+}
+
+.search-box-list {
+    position: relative;
+    display: block;
+}
+
+.dropbtn {
+    background-color: #1c1c22;
+    border-color: #1c1c22;
+    color: white;
+    text-align: right;
+    border: none;
+    width: 100%;
+    cursor: pointer;
+    border-radius: 5px;
+    padding: 10px 20px;
+    font-size: 14px;
+    position: relative;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    line-height: 2;
+    white-space: nowrap;
+    vertical-align: middle;
+    user-select: none;
+    align-items: center;
+}
+
+.angle-left-icon {
+    font-size: 14px;
+    color: #fff;
+    display: block;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #1c1c22;
+    border-color: #1c1c22;
+    margin-top: 2px !important;
+    backdrop-filter: blur(4px);
+    width: 100%;
+    z-index: 999;
+    padding: 15px;
+    border-radius: 5px;
+    max-height: 420px;
+    overflow-y: auto !important;
+}
+
+//checkbox
+
+.custom-radio {
+    display: block;
+    position: relative;
+    cursor: pointer;
+    line-height: 20px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    align-items: center;
+
+
+    input {
+        opacity: 0;
+        cursor: pointer;
+        margin-right: 10px;
+    }
+}
+
+.text-item {
+    margin-bottom: 10px;
+    color: #bbc1c6;
+}
+
+.checkmark {
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 15px;
+    width: 15px;
+    background-color: #eee;
+    border-radius: 50%;
+}
+
+.custom-radio:hover input~.checkmark {
+    background-color: #ccc;
+}
+
+.custom-radio input:checked~.checkmark {
+    background-color: #2196F3;
+}
+
+.checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+}
+
+.custom-radio input:checked~.checkmark:after {
+    display: block;
+}
+
+
 </style>
