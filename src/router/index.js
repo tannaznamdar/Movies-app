6f773db7719434b28fff442ca45d9ac067cc40f3
Alireza <alirezamires@gmail.com>
2024-04-24 23:37:09 +0330

    add home page / title page

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import PersianDub from "@/views/PersianDubPage.vue";
 import Other from "@/views/OtherPage.vue";
 import Job from "@/views/JobPage.vue";
 import ContactUs from "@/views/ContactUsPage.vue";
+import Home from "@/views/HomePage.vue";
+import TitlePage from "@/views/TitlePage.vue";
 import Login from "@/views/LoginPage.vue";
 
 
@@ -15,16 +17,18 @@ import Login from "@/views/LoginPage.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   
+    {path: "/",name: 'homePageRoute',component: Home},
+    {path: "/title/:slug",name: 'titlePageRoute',component: TitlePage},
     {path: "/genre/:genre",name: 'moviesGenrePageRoute',component: MoviesGenre},
     {path: "/movie/:country",name: 'countryMoviePageRoute',component: CountryMovies},
     {path: "/series/:country",name: 'countrySeriesPageRoute',component: CountrySeries},
     {path:"/animation", name:"animationPageRoute" , component: Animation},
-    {path:"/persiandub", name:"persianDubPageRoute" , component: PersianDub},
+    {path:"/persian-dub", name:"persianDubPageRoute" , component: PersianDub},
     {path: "/other/:content",name: 'otherPageRoute',component: Other},
     {path:"/job", name:"jobPageRoute" , component: Job},
-    {path:"/contactus", name:"contactUsPageRoute" , component: ContactUs},
+    {path:"/contact-us", name:"contactUsPageRoute" , component: ContactUs},
     {path:"/login", name:"loginPageRoute" , component: Login},
+    { path: "/:catchAll(.*)", redirect: '/' }
   ]
 })
 
