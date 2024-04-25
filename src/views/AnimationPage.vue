d545e1fb5a249c76b9b473dfd13e84bac7a6309d
tannaznamdar <tannaznamdar99@gmail.com>
2024-04-25 10:16:36 +0330

    update home page & Animation Page

--- src/views/AnimationPage.vue
+++ src/views/AnimationPage.vue
@@ -1,98 +1,22 @@
 <template>
-
-  <!-- <h1> Animation Page</h1> -->
-
-  <div class="container-fluid">
-    <section class="movie-card">
-      <div class="container bv-example-row">
-
-        <div class="row">
-          <div class=" mt-5 mb-3 d-flex align-items-center justify-content-between">
-            <h4 class="title">ایرانی رایگان</h4>
-            <button class="button button--transparent"><a href="#"> مشاهده همه </a></button>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-2" v-for="movieCard in movieCards">
-            <MovieCards v-bind="movieCard"></MovieCards>
-          </div>
-        </div>
-
-      </div>
-    </section>
-
+  <div>
+    <h1> Animation Page</h1>
   </div>
-
 </template>
+
 
 <script>
 
-import MovieCards from "@/components/MovieCards.vue";
-
 export default {
   name: 'Animation',
-  components: {MovieCards},
+
 
   data() {
     return {
-
-      movieCards: [
-        {
-          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2018/06/shahrzad-s3-final-min-214x300.jpg',
-          link: 'thumbnail',
-          genre: ' درام,تاریخی,رمانتیک',
-          imdb: '6.3',
-          title_fa: 'شهرزاد',
-        },
-        {
-          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2020/09/del-40-207x290.jpg',
-          link: 'thumbnail',
-          genre: ' درام,رمانتیک',
-          imdb: '7.2',
-          title_fa: 'سریال دل ',
-        },
-        {
-          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2019/03/Metri6.5-207x290.jpg',
-          link: 'thumbnail',
-          genre: 'اکشن,جنایی,درام',
-          imdb: '8.1',
-          title_fa: 'متری شیش و نیم',
-        },
-        {
-          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2024/04/GonahFereshteh-18-207x290.jpg',
-          link: 'thumbnail',
-          genre: ' درام, خانوادگی',
-          imdb: '7.3',
-          title_fa: 'گناه فرشته  ',
-        },
-        {
-          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2024/04/Jangal-Asphalt-5-207x290.jpg',
-          link: 'thumbnail',
-          genre: ' درام,رمانتیک',
-          imdb: '8.3',
-          title_fa: 'افعی تهران',
-        },
-        {
-          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2018/06/shahrzad-s3-final-min-214x300.jpg',
-          link: 'thumbnail',
-          genre: ' درام,تاریخی,رمانتیک',
-          imdb: '6.3',
-          title_fa: 'جنگل اسفالت  ',
-        },
-      ]
     }
   }
 
 }
-
 </script>
 
-<style scoped>
-
-.movie-card {
-  background-color: #16161b;
-}
-
-
-</style>
+<style scoped></style>
