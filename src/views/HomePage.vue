998a3c12ede6f76fd6bb8487c844479e50095f9f
tannaznamdar <tannaznamdar99@gmail.com>
2024-04-26 22:18:03 +0330

    create new section in home page (new Persian Series)

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,7 +11,7 @@ import MovieCards from "@/components/MovieCards.vue";
 
         <div class="row">
           <div class=" mt-5 mb-3 d-flex align-items-center justify-content-between">
-            <h4 class="title">غیلم ایرانی رایگان</h4>
+            <h4 class="title">فیلم ایرانی رایگان</h4>
             <button class="button button--transparent"><a href="#"> مشاهده همه </a></button>
           </div>
         </div>
@@ -61,6 +61,26 @@ import MovieCards from "@/components/MovieCards.vue";
           aria-label="My Favorite Images">
           <SplideSlide v-for="internationalSerial in internationalSeries">
             <MovieCards v-bind="internationalSerial"></MovieCards>
+          </SplideSlide>
+        </Splide>
+
+      </div>
+    </section>
+
+    <section class="movie-card">
+      <div class="container bv-example-row">
+        <div class="row">
+          <div class=" mt-5 mb-3 d-flex align-items-center justify-content-between">
+            <h4 class="title"> سریال ایرانی جدید </h4>
+            <button class="button button--transparent"><a href="#"> مشاهده همه </a></button>
+          </div>
+        </div>
+
+        <Splide class="row"
+          :options="{ arrows: false, pagination: false, direction: 'rtl', type: 'slide', trimSpace: false, perPage: 6, perMove: 1 }"
+          aria-label="My Favorite Images">
+          <SplideSlide v-for="newPersianSerial in newPersianSeries">
+            <MovieCards v-bind="newPersianSerial"></MovieCards>
           </SplideSlide>
         </Splide>
 
@@ -405,6 +425,7 @@ export default {
           hasTag: false,
           tag: {
             title: ' رایگان ',
+            small: true,
           }
         },
         {
@@ -419,6 +440,7 @@ export default {
           hasTag: false,
           tag: {
             title: ' رایگان ',
+            small: true,
           }
         },
         {
@@ -478,6 +500,7 @@ export default {
           hasTag: false,
           tag: {
             title: ' رایگان ',
+            small: true,
           }
         },
         {
@@ -492,6 +515,7 @@ export default {
           hasTag: false,
           tag: {
             title: ' رایگان ',
+            small: true,
           }
         },
         {
@@ -525,6 +549,174 @@ export default {
           }
         },
 
+      ],
+
+      newPersianSeries: [
+        {
+          slug: 'شهرزاد',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2018/06/shahrzad-s3-final-min-214x300.jpg',
+          link: 'thumbnail',
+          genre: ' درام,تاریخی,رمانتیک',
+          imdb: '6.3',
+          title: 'شهرزاد',
+          subtitle: 'فصل اول قسمت هفتم',
+          hasSubtitle: true,
+          hasTag: true,
+          tag: {
+            title: ' جمعه ها ',
+            small: true,
+          }
+        },
+        {
+          slug: 'دل',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2020/09/del-40-207x290.jpg',
+          link: 'thumbnail',
+          genre: ' درام,رمانتیک',
+          imdb: '7.2',
+          title: 'سریال دل ',
+          subtitle: 'فصل اول قسمت هفتم',
+          hasSubtitle: true,
+          hasTag: true,
+          tag: {
+            title: ' پنجشنبه ها ',
+            small: true,
+          }
+        },
+        {
+          slug: 'متری-شیش-و-نیم',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2019/03/Metri6.5-207x290.jpg',
+          link: 'thumbnail',
+          genre: 'اکشن,جنایی,درام',
+          imdb: '8.1',
+          title: 'متری شیش و نیم',
+          subtitle: 'فصل اول قسمت هفتم',
+          hasSubtitle: true,
+          hasTag: true,
+          tag: {
+            title: ' شنبه ها ',
+            small: true,
+          }
+        },
+        {
+          slug: 'گناه-فرشته',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2024/04/GonahFereshteh-18-207x290.jpg',
+          link: 'thumbnail',
+          genre: ' درام, خانوادگی',
+          imdb: '7.3',
+          title: 'گناه فرشته  ',
+          subtitle: 'فصل اول قسمت هفتم',
+          hasSubtitle: true,
+          hasTag: true,
+          tag: {
+            title: ' یکشنبه ها ',
+            small: true,
+          }
+        },
+        {
+          slug: 'افعی-تهران',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2024/04/Viper-of-Tehran-8-207x290.jpg',
+          link: 'thumbnail',
+          genre: ' درام,رمانتیک',
+          imdb: '8.3',
+          title: 'افعی تهران',
+          subtitle: 'فصل اول قسمت هفتم',
+          hasSubtitle: true,
+          hasTag: true,
+          tag: {
+            title: '  شنبه ها  ',
+            small: true,
+          }
+        },
+        {
+          slug: 'جنگل-آسفالت',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2024/04/Jangal-Asphalt-5-207x290.jpg',
+          link: 'thumbnail',
+          genre: ' درام,تاریخی,رمانتیک',
+          imdb: '6.3',
+          title: 'جنگل آسفالت  ',
+          subtitle: 'فصل اول قسمت هفتم',
+          hasSubtitle: true,
+          hasTag: false,
+          tag: {
+            title: ' ',
+            small: true,
+          }
+        },
+        {
+          slug: 'شهرزاد',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2018/06/shahrzad-s3-final-min-214x300.jpg',
+          link: 'thumbnail',
+          genre: ' درام,تاریخی,رمانتیک',
+          imdb: '6.3',
+          title: 'شهرزاد',
+          subtitle: 'فصل اول قسمت هفتم',
+          hasSubtitle: true,
+          hasTag: false,
+          tag: {
+            title: '  ',
+            small: true,
+          }
+        },
+        {
+          slug: 'دل',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2020/09/del-40-207x290.jpg',
+          link: 'thumbnail',
+          genre: ' درام,رمانتیک',
+          imdb: '7.2',
+          title: 'سریال دل ',
+          subtitle: 'فصل اول قسمت هفتم',
+          hasSubtitle: true,
+          hasTag: true,
+          tag: {
+            title: ' پنجشنبه ها ',
+            small: true,
+          }
+        },
+        {
+          slug: 'گناه-فرشته',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2024/04/GonahFereshteh-18-207x290.jpg',
+          link: 'thumbnail',
+          genre: ' درام, خانوادگی',
+          imdb: '7.3',
+          title: 'گناه فرشته  ',
+          subtitle: '',
+          hasSubtitle: false,
+          hasTag: false,
+          tag: {
+            title: ' رایگان ',
+            small: true,
+          }
+        },
+        {
+          slug: 'افعی-تهران',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2024/04/Viper-of-Tehran-8-207x290.jpg',
+          link: 'thumbnail',
+          genre: ' درام,رمانتیک',
+          imdb: '8.3',
+          title: 'افعی تهران',
+          subtitle: 'فصل اول قسمت هفتم',
+          hasSubtitle: true,
+          hasTag: false,
+          tag: {
+            title: ' ',
+            small: true,
+          }
+        },
+        {
+          slug: 'جنگل-آسفالت',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2024/04/Jangal-Asphalt-5-207x290.jpg',
+          link: 'thumbnail',
+          genre: ' درام,تاریخی,رمانتیک',
+          imdb: '6.3',
+          title: 'جنگل آسفالت  ',
+          subtitle: 'فصل اول قسمت هفتم',
+          hasSubtitle: true,
+          hasTag: true,
+          tag: {
+            title: ' پنجشنبه ها ',
+            small: true,
+          }
+        },
       ],
 
     }
