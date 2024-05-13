ae6697a366cd730983b3188481431b17dd74c57b
tannaznamdar <tannaznamdar99@gmail.com>
2024-05-13 10:41:35 +0330

    create Collection Page :)

--- src/views/CollectionPage.vue
+++ src/views/CollectionPage.vue
@@ -7,6 +7,7 @@ import collectioncards from "@/components/CollectionCards.vue";
   <div class="container-fluid">
     <section>
       <div class="container bv-example-row">
+        <h4 class="archive-title mt-5 mb-4">آرشیو کالکشن ها</h4>
         <div class="row">
           <div class=" col-lg-3" v-for="collectionMovie in collectionMovies">
             <collectioncards v-bind="collectionMovie"></collectioncards>
@@ -34,6 +35,124 @@ export default {
           title: 'Pet Sematary',
           subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
         },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+        {
+          slug: 'Pet-Sematary',
+          thumbnail: 'https://www.uptvs.com/wp-contents/uploads/2023/12/Pet-Sematary-2-2023-207x290.jpg',
+          link: 'thumbnail',
+          Imdb: '6.3',
+          episode: 2,
+          title: 'Pet Sematary',
+          subtitle: 'در سال ۱۹۶۹، جاد کرندال جوان آرزو دارد از زادگاه خود لودلا برود اما متوجه',
+        },
+
 
       ]
     }
@@ -43,4 +162,11 @@ export default {
 
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.archive-title {
+  font-size: 17px;
+  line-height: 1.3;
+  font-weight: 700;
+  color: #bbc1c6;
+}
+</style>
