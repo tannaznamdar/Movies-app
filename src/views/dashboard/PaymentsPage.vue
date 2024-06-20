64d74dc70c21bdf18d5267bdb660be57183d1ce5
tannaznamdar <tannaznamdar99@gmail.com>
2024-06-20 19:45:41 +0330

    Responsive Payments page

--- src/views/dashboard/PaymentsPage.vue
+++ src/views/dashboard/PaymentsPage.vue
@@ -12,7 +12,7 @@ import comments from '@/assets/images/comments.svg'
 <template>
     <div class="pt-150 pb-5 container bv-example-row">
         <div class="row">
-            <div class="col-lg-3">
+            <div class="col-lg-3 col-md-4 mb-15">
                 <div class="sidebar_dashboard">
                     <div class="side_head_dashboard">
                         <div class="inner_side_head_dashboard">
@@ -145,11 +145,11 @@ import comments from '@/assets/images/comments.svg'
                 </div>
             </div>
 
-            <div class="col-lg-9">
+            <div class="col-lg-9 col-md-8">
                 <div class="main_dashboard">
                     <div class="before_msg mb-4" v-if="hasBeforeMsgAlert">
-                        <div class="item_dashboard danger d-flex justify-content-between">
-                            <span class="alert"> کاربر گرامی برای بهره‌مندی از همه
+                        <div class="item_dashboard danger d-flex justify-content-between direction-column">
+                            <span class="alert mb-15"> کاربر گرامی برای بهره‌مندی از همه
                                 امکانات سایت لطفاً آدرس ایمیل خود را تایید کنید.
                             </span>
                             <span class="alert-btn">
@@ -234,187 +234,29 @@ export default {
 
 
 <style scoped lang="scss">
+.container {
+    @media (max-width:820px) {
+        max-width: 99%;
+    }
+}
+
 .pt-150 {
     padding-top: 150px;
 
-    @media (max-width:449px) {
+    @media (max-width:820px) {
         padding-top: 120px;
     }
-}
-
-.sidebar_dashboard {
-    min-height: 820px;
-    width: 100%;
-    border-radius: 14px;
-    background: #23232b;
-    overflow: hidden;
-}
-
-.inner_side_head_dashboard {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    background: #6e7072;
-    position: relative;
-    text-align: center;
-    width: 100%;
-    height: 126px;
-}
-
-.avatar_user_panel {
-    width: 89px;
-    height: 89px;
-    line-height: 93px;
-    font-size: 250%;
-    text-align: center;
-    color: #fff;
-    border-radius: 100%;
-    background-color: #6898f8;
-    box-shadow: 0 0 29px 0 rgba(88, 83, 167, 0.4);
-    position: absolute;
-    top: 70px;
-    left: 0;
-    right: 0;
-    margin-right: auto;
-    margin-left: auto;
-}
-
-.wellcome_message {
-    display: inline-block;
-    width: 100%;
-    text-align: center;
-    color: #fff;
-    margin-top: 55px;
-    font-size: 16px;
-    font-weight: 300;
-}
-
-.user_name {
-    font-size: 14px;
-    font-weight: 500;
-    display: inline-block;
-    width: 100%;
-    text-align: center;
-    color: #6898f8;
-    margin-top: 5px;
-    margin: 0;
-}
-
-.btn_show_profile {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 10px;
-    margin-top: 15px;
-
-    a {
-        text-decoration: none;
-        font-size: 14px;
-        display: inline-block;
-        padding: 10px 15px;
-        background: #050505;
-        box-shadow: 0 0 10px rgba(0, 0, 0, .7);
-        color: #fff;
-        border-radius: 35px;
-        line-height: normal;
-    }
-}
-
-.dashboard_menu_nav {
-    display: inline-block;
-    width: 100%;
-    margin-top: 10px;
-
-    ul {
-        list-style: none;
-        padding: 0;
-
-        li {
-            float: right;
-            width: 100%;
-
-            a {
-                font-weight: 400;
-                font-size: 16px;
-                text-decoration: none;
-                display: inline-block;
-                padding: 20px;
-                color: #fff;
-                width: 100%;
-                position: relative;
-                transition: .3s;
-                border-right: solid;
-                border-width: 3px;
-                border-color: #23232b;
-
-                i {
-                    font-size: 14PX;
-                    color: #6e7072;
-                }
-
-                &:is(:hover, :focus) {
-                    background: #0a0a0a;
-                    border-color: #6898f8;
-                }
-            }
-        }
-    }
-}
-
-.right_icon {
-    float: right;
-    margin-left: 8px;
-}
-
-.main_dashboard {
-    border-radius: 14px;
-    background-color: #23232b;
-    color: #fff;
-    min-height: 820px;
-    padding: 20px;
-}
-
-.item_dashboard {
-    display: inline-block;
-    width: 100%;
-    margin-bottom: 10px;
-    background-color: #1c1c22;
-    box-shadow: 0 4px 0 0 rgba(0, 0, 0, .4);
-    padding: 20px;
-    border-radius: 5px;
-    border-right: 5px;
-}
-
-.danger {
-    background: #910909;
-    box-shadow: 0 4px 0 0 #91090900;
-}
-
-.alert {
-    font-size: 15px;
-    font-weight: 300;
-    line-height: normal;
-    margin: 0;
-    padding: 0;
-}
-
-.alert-btn a {
-    text-decoration: none;
-    border-radius: 35px;
-    background: #6898f8;
-    color: #fff;
-    font-size: 90%;
-    padding: 7px 20px;
-    font-size: 13px;
-    font-weight: 300;
 }
 
 .text-14 {
     font-size: 14px;
     font-weight: 500;
     margin: 0;
+
+    @media (max-width:820px) {
+        font-size: 12px;
+        line-height: 2;
+    }
 
     a {
         text-decoration: none;
@@ -471,5 +313,18 @@ th {
     margin-top: -5px;
     font-size: 18px;
     font-weight: 400;
+}
+
+.direction-column {
+    @media (max-width:820px) {
+        display: flex;
+        flex-direction: column;
+    }
+}
+
+.mb-15 {
+    @media (max-width:820px) {
+        margin-bottom: 15px;
+    }
 }
 </style>
